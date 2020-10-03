a0843ac94b560ab9bb49c67e9c979ca59eda4677
Jeremy Marino <jermarino16@gmail.com>
2020-10-03 10:45:08 -0700

    on exercise 2 part 2 of frontendmasters

--- app/router.js
+++ app/router.js
@@ -1,12 +1,35 @@
-import EmberRouter from '@ember/routing/router';
-import config from './config/environment';
+import EmberRouter from "@ember/routing/router";
+import config from "./config/environment";
 
 const Router = EmberRouter.extend({
   location: config.locationType,
-  rootURL: config.rootURL
+  rootURL: config.rootURL,
 });
 
-Router.map(function() {
+Router.map(function () {
+  this.route("orgs");
+
+  this.route(
+    "org",
+    {
+      path: "org/:id",
+    },
+    function () {
+      this.route("repos");
+
+      this.route(
+        "repo",
+        {
+          path: ":repoid",
+        },
+        function () {
+          this.route("contributors");
+          this.route("issues");
+        }
+      );
+    }
+  );
+  this.route("notfound", { path: "*path" });
 });
 
 export default Router;
